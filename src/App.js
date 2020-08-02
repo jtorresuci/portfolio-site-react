9493501d64f1432a22c2e603e030e4e336e643b6
Jose Torres <jtorresuci@gmail.com>
2020-08-01 21:16:09 -0700

    Added resume pdf to Resume component using react-pdf. Installedreact-pdf with: npm install --save react-pdf.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
            {/*<h3>Top level routes</h3>*/}
 
            <nav className="navbar navbar-expand-lg navbar-light bg-light header-color">
-           <a className="navbar-brand " href="#">JOSE TORRES</a>
+           <a className="navbar-brand " href="/">JOSE TORRES</a>
                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                    <span className="navbar-toggler-icon"></span>
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import Pdf from '../pdf/resume-web-aug-1.pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+
 
 function Resume (){
+
     return(
-        <div>
-            <h2>Resume</h2>
-        </div>
+
+            <Document file={Pdf}>
+                <Page pageNumber={1}/>
+            </Document>
+
 
     )
 }
