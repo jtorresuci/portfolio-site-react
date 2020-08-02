9b44a42663cac7daa141230a14bb546acf23b2b9
Jose Torres <jtorresuci@gmail.com>
2020-08-01 23:23:26 -0700

    Finished the site, can continue and improve it. For example the Projects page should load an initial set of cards first.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import './Contact.css'
 function Contact (){
     return(
-        <div>
-            <h2>Contact</h2>
+        <div >
+            <h1 className={"text-body"}>Contact Me</h1>
+            <div className="contact-border"></div>
+            <h1 className={"text-body"}>Phone: 949-303-0202</h1>
+            <h1 className={"text-body"}>Email: jtorresuci@gmail.com</h1>
         </div>
 
     )
