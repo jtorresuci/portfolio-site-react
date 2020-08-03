debbd17aabf176439344ea264d7fe64ae51d76d7
Jose Torres <jtorresuci@gmail.com>
2020-08-03 11:39:23 -0700

    Site now loads ReactJS cards on initial load rather than emptty page. Jose Torres link is now fixed and does not 404 anymore.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Route } from 'react-router-dom';
 import Python from './lang/Python'
 import ReactJS from './lang/ReactJS'
@@ -6,7 +6,11 @@ import Swift from './lang/Swift'
 import './Projects.css'
 
 function Projects (){
-    const tabs = ["ReactJS", "Swift", "Python"]
+    const tabs = ["ReactJS", "Swift", "Python"];
+    const [load, setLoad] = useState(true);
+
+
+
     return(
         <div>
 
@@ -14,13 +18,18 @@ function Projects (){
                 {tabs.map((tab, index) => {
                     return (
                         <li >
-                            <Link to ={`/Projects/${tab}`} class="tabs-text">{tab}</Link>
+                            <Link to ={`/Projects/${tab}`} class="tabs-text" onClick={() => setLoad(false)}>{tab}</Link>
                             <div class="link-border"></div>
                         </li>
                     )
                 })}
             </ul>
-            <Route path={'/Projects/ReactJS'} component={ReactJS}></Route>
+            {load ? (
+                <Route path={'/Projects/'} component={ReactJS}></Route>
+
+            ) : (
+                <Route path={'/Projects/ReactJS'} component={ReactJS}></Route>
+            )}
             <Route path={'/Projects/Swift'} component={Swift}></Route>
             <Route path={'/Projects/Python'} component={Python}></Route>
         </div>
