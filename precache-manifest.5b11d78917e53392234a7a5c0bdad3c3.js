5c2117031ff4a583b8b924992041d3c2c72e3ba1
Jose Torres <jtorresuci@gmail.com>
2020-08-03 13:15:49 -0700

    Updates

---
+++ precache-manifest.5b11d78917e53392234a7a5c0bdad3c3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd06f9f7df646bf1e62f596f110defa",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "c9f015d610003e5e7d89",
+    "url": "/portfolio-site/static/css/main.0799582e.chunk.css"
+  },
+  {
+    "revision": "d5f3ac036fc1e54c9c94",
+    "url": "/portfolio-site/static/js/2.92e86e58.chunk.js"
+  },
+  {
+    "revision": "81d9b24e0b22758e373574b225b8549b",
+    "url": "/portfolio-site/static/js/2.92e86e58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f015d610003e5e7d89",
+    "url": "/portfolio-site/static/js/main.55f341ec.chunk.js"
+  },
+  {
+    "revision": "3f74fff967dd2f7ba226",
+    "url": "/portfolio-site/static/js/runtime-main.9526c994.js"
+  },
+  {
+    "revision": "4822283c58ccfb34868b437fe4fedc7a",
+    "url": "/portfolio-site/static/media/93.4822283c.png"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "/portfolio-site/static/media/prnovabold.62d4d7d3.otf"
+  },
+  {
+    "revision": "56b54001dd42f5fb8eba8af36465b364",
+    "url": "/portfolio-site/static/media/prnovathin.56b54001.otf"
+  },
+  {
+    "revision": "7107e0a631fa2d3bf8645cfe9d78ec5d",
+    "url": "/portfolio-site/static/media/resume-web-aug-1.7107e0a6.pdf"
+  }
+]);
